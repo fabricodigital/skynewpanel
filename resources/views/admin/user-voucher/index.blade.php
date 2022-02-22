1dceb419ccf5720e4ec17316044739ef3a844417
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-22 04:05:15 -1200

    Update index.blade.php

--- resources/views/admin/user-voucher/index.blade.php
+++ resources/views/admin/user-voucher/index.blade.php
@@ -7,16 +7,20 @@
 
 @include('partials._alerts')
 
-<div class="row ">
-    <div class="col-md-12">
-        <form class="" action="#" method="get">
-            <div class="form-group row text-center">
-                <input class="form-control" type="text" name="search" required/>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <div class="row ">
+            <div class="col-md-12">
+                <form class="" action="#" method="get">
+                    <div class="form-group row text-center">
+                        <input class="form-control" type="text" name="search" required/>
+                    </div>
+                    <div class="form-group row text-center">
+                        <button class="form-control btn btn-success" type="submit">Search</button>
+                    </div>
+                </form>
             </div>
-            <div class="form-group row text-center">
-                <button class="form-control btn btn-success" type="submit">Search</button>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
