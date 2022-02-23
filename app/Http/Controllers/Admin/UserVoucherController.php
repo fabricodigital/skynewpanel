f084ebb52fc7e3bbf153c0cec73c7e0f2165a892
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-22 21:29:39 -1200

    Update UserVoucherController.php

--- app/Http/Controllers/Admin/UserVoucherController.php
+++ app/Http/Controllers/Admin/UserVoucherController.php
@@ -23,25 +23,10 @@ class UserVoucherController extends Controller
             $uservouch =  UserVoucher::where('idSky', '=', $search)->orWhere('codicefiscale', '=', $search)->first();
 
             $getPromotions = DB::select(
-                "SELECT distinct sv.codice,u.lista,u.cluster,u.tempo_contratto,e.nome,tipologia_abbonamento_new.tipologia,u.`idSky`,u.promozione,pd.descrizione as checknewpromo, 
-            (select codice from codici where idSky=$uservouch->idSky and type = 14545  LIMIT 0,1) as checknewpromo3
-
-            FROM 'elencopromozioni' e
-
-            INNER JOIN `utentivoucher` u ON e.`abbr` = u.`promozione` AND u.`idSky` = $uservouch->idSky
-
-            left join promodescrizione pd ON e.id = pd.promozione
-
-            left join stampavoucher sv on u.idSky = sv.idSky and sv.promozione = u.`promozione` and sv.prodotto=u.prodotto
-
-            join tipologia_abbonamento_new  on u.prodotto = tipologia_abbonamento_new.id
-
-            WHERE e.`attivoadmin` = 'si' AND NOW()>=e.datainizio and tipologia_abbonamento_new.datafine IS NULL  AND e.datafine >= NOW()  
-
-            group by tipologia_abbonamento_new.id
-
-            ORDER BY tipologia_abbonamento_new.posizione ASC,e.id desc,tipologia_abbonamento_new.nome asc"
+                "SELECT * FROM elencopromozioni"
             );
+
+            return $getPromotions;
 
         }
     }
