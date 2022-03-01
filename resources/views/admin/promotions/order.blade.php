72d706821be9fb986f99af409739ce95d01450ae
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-28 21:21:26 -1200

    Update order.blade.php

--- resources/views/admin/promotions/order.blade.php
+++ resources/views/admin/promotions/order.blade.php
@@ -1,31 +1,15 @@
 @extends('layouts.admin')
 
-@section('title', $promotion::getMsgTrans('view_heading'))
+@section('title', App\Models\Admin\Promotion::getTitleTrans())
 
 @section('content')
-    @include('partials._content-heading', ['title' => $promotion::getMsgTrans('view_heading'), 'deleted' => !empty($promotion->deleted_at) ? true : false])
 
-    <div class="panel panel-default{{ !empty($promotion->deleted_at) ? ' record-deleted' : '' }}">
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-md-12">
+    @include('partials._content-heading', ['title' => App\Models\Admin\Promotion::getTitleTrans()])
 
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('partials._alerts')
 
-    <div class="pull-left">
-
-    </div>
-
-    <div class="pull-right">
-        <a href="{{ route('admin.promotions.index') }}" class="btn btn-primary">@lang('Back')</a>
-    </div>
-
-    <div class="clearfix"></div>
-
-
+   
 
 @stop
+
 
