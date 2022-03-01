136cce2842adf9cc16a97c3a195d5a0835e2812a
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-28 21:38:50 -1200

    Update order.blade.php

--- resources/views/admin/promotions/order.blade.php
+++ resources/views/admin/promotions/order.blade.php
@@ -3,25 +3,25 @@
 @section('title', App\Models\Admin\Promotion::getTitleTrans())
 
 @section('content')
-<style>
-    [draggable] {
-        -moz-user-select: none;
-        -khtml-user-select: none;
-        -webkit-user-select: none;
-        user-select: none;
-        /* Required to make elements draggable in old WebKit */
-        -khtml-user-drag: element;
-        -webkit-user-drag: element;
-    }
-</style>
+
     @include('partials._content-heading', ['title' => App\Models\Admin\Promotion::getTitleTrans()])
 
     @include('partials._alerts')
-
+    <style>
+        [draggable] {
+            -moz-user-select: none;
+            -khtml-user-select: none;
+            -webkit-user-select: none;
+            user-select: none;
+            /* Required to make elements draggable in old WebKit */
+            -khtml-user-drag: element;
+            -webkit-user-drag: element;
+        }
+    </style>
     <div class="panel panel-default">
         <div class="panel-body">
             <div class="row">
-                <div class="col-lg-10"   >
+                <div class="col-lg-10"  id="columns" >
                     @foreach($promotions as $p)
                         <div class="alert alert-success column" draggable="true" role="alert" style=" min-height: 72px ;border: 4px solid #00a65a !important;background-color: transparent !important;color: black !important;">
                             <p class="pull-left">{{$p->nome}} </p> <p class="pull-right">{{$p->datafine}}</p>
