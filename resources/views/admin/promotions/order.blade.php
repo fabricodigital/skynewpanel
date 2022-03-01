03b304c762dc5e140baceecc52cbdddf76e9da86
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-28 21:23:28 -1200

    Update order.blade.php

--- resources/views/admin/promotions/order.blade.php
+++ resources/views/admin/promotions/order.blade.php
@@ -8,7 +8,15 @@
 
     @include('partials._alerts')
 
-   
+    <div class="panel panel-default">
+        <div class="panel-body">
+            @foreach($promotions as $p)
+                <div class="alert alert-success" role="alert">
+                    <p>{{$p->nome}}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 @stop
 
