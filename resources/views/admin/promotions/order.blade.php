93fe22f7ff45096d9913dc3f6a18ef585872239e
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-28 21:35:04 -1200

    Update order.blade.php

--- resources/views/admin/promotions/order.blade.php
+++ resources/views/admin/promotions/order.blade.php
@@ -11,9 +11,9 @@
     <div class="panel panel-default">
         <div class="panel-body">
             <div class="row">
-                <div class="col-lg-10">
+                <div class="col-lg-10"  id="image-list">
                     @foreach($promotions as $p)
-                        <div class="alert alert-success" role="alert" style=" min-height: 72px ;border: 4px solid #00a65a !important;background-color: transparent !important;color: black !important;">
+                        <div class="alert alert-success" id="img" role="alert" style=" min-height: 72px ;border: 4px solid #00a65a !important;background-color: transparent !important;color: black !important;">
                             <p class="pull-left">{{$p->nome}} </p> <p class="pull-right">{{$p->datafine}}</p>
                         </div>
                     @endforeach
