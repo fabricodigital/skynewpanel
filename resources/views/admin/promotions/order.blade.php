0c98bade5350e3e3d3bc56806b23421d1c76034e
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-28 21:32:08 -1200

    Update order.blade.php

--- resources/views/admin/promotions/order.blade.php
+++ resources/views/admin/promotions/order.blade.php
@@ -11,7 +11,7 @@
     <div class="panel panel-default">
         <div class="panel-body">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-10">
                     @foreach($promotions as $p)
                         <div class="alert alert-success" role="alert" style=" height: 72px ;border: 4px solid #00a65a !important;background-color: transparent !important;color: black !important;">
                             <p class="pull-left">{{$p->nome}} </p> <p class="pull-right">{{$p->datafine}}</p>
