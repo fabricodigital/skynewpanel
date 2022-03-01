e47fc0d5e46bd44ee8906193ff11c96708f7e4bf
stefano vardabasso <stefanovardabasso@gmail.com>
2022-02-28 21:41:54 -1200

    Update order.blade.php

--- resources/views/admin/promotions/order.blade.php
+++ resources/views/admin/promotions/order.blade.php
@@ -21,13 +21,28 @@
     <div class="panel panel-default">
         <div class="panel-body">
             <div class="row">
-                <div class="col-lg-10"  id="columns" >
-                    @foreach($promotions as $p)
-                        <div class="alert alert-success column" draggable="true" role="alert" style=" min-height: 72px ;border: 4px solid #00a65a !important;background-color: transparent !important;color: black !important;">
-                            <p class="pull-left">{{$p->nome}} </p> <p class="pull-right">{{$p->datafine}}</p>
+                <ul id="image-list">
+                    @foreach($promotions as $im)
+
+                        <div class="col-sm-6 col-md-4" style="width: 200px" id="img{{$im->id}}"  >
+                            <div class="thumbnail">
+                                <li style="color: transparent;" class="lis"  id="image_{{$im->id}}">
+                                    <button onclick="deleteimage({{$im->id}})" class="delete-file"   style="background: rgb(255, 4, 4);padding: 0.09rem 0.25em 0px;text-align: center;
+                               border-radius: 50%;z-index: 199;color: rgb(255, 255, 255);"><i class="glyphicon glyphicon-remove"></i></button>
+                                    <p style="width: 150px; height: 150px;" class="img-responsive" alt=""  >{{$im->nome}}</p>
+                                </li>
+                            </div>
                         </div>
+
                     @endforeach
-                </div>
+                </ul>
+{{--                <div class="col-lg-10"  id="columns" >--}}
+{{--                    @foreach($promotions as $p)--}}
+{{--                        <div class="alert alert-success column" draggable="true" role="alert" style=" min-height: 72px ;border: 4px solid #00a65a !important;background-color: transparent !important;color: black !important;">--}}
+{{--                            <p class="pull-left">{{$p->nome}} </p> <p class="pull-right">{{$p->datafine}}</p>--}}
+{{--                        </div>--}}
+{{--                    @endforeach--}}
+{{--                </div>--}}
             </div>
 
         </div>
